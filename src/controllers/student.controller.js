5986d9849ec7a987e185d5cb8f7c78294192a6f5
Deepak Sharma <deepakmyside@gmail.com>
2026-09-19 19:41:06 +0530

    feat: add student controller

---
+++ src/controllers/student.controller.js
@@ -0,0 +1,40 @@
+const prisma = require("../config/db")
+
+const getStudents = async (req, res) => {
+    try{
+        const { branchId, semesterId, section } = req.query;
+
+        const  students = await prisma.students.findMany({
+            where: {
+                branchId: Number(branchId),
+                semesterId: Number(semesterId),
+                section,
+            },
+            select: {
+                id: true,
+                uid: true,
+                name: true,
+                batch: true,
+                section: true,
+            },
+            orderBy: {
+                name: "asc",
+            },
+        });
+
+        res.status(200).json({
+            success: true,
+            count: students.length,
+            students,
+        });
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            success: false,
+            message: "Failed to fetch students"
+        });
+    }
+};
+
+module.exports = { getStudents,}
